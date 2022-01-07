7ba9d54ef9887fa0444412e6c434467ef143e76e
hz153 <1015528705@qq.com>
2022-01-07 23:30:37 +0800

    prolog practice

--- kb5_1.pl
+++ kb5_1.pl
@@ -51,4 +51,57 @@ class(X, winner):-
 beat(X, _), !.
 class(X, sportsman):-
 beat(_, X).
+add1(X, List, [X|List]).
+% 练习5-1
+% p(X),!,p(Y)在这个情况下，p(X)被匹配后就被确定了，不会再进行回溯
+p(1).
+p(2):- !.
+p(3).
+% 练习5-2
+class1(Number, positive):-
+Number>0, !.
+class1(0, zero):- !.
+class1(Number, negtive).
+% 练习5-3
+split([],[],[]).
+% 通过增加cut后，可以省去判断X为negtive（因为是互斥的情况），从而提高程序效率
+split([X|Numbers], Positive, Negative):-
+class1(X, Y), 
+Y \= negtive, !,
+add1(X, Positive1, Positive),
+split(Numbers, Positive1, Negative).
 
+split([X|Numbers], Positive, Negative):-
+add1(X, Negative1, Negative),
+split(Numbers, Positive, Negative1).
+
+% 如果需要表示mary喜欢除了蛇的所有动物，就需要使用cut+否定
+% cut可以用于表示存在
+% prolog中fail和false都可以表示真值为假
+different(X, X):-!, fail.
+
+different(X, Y).
+% 写not逻辑，注意，这个P不能是原子命题，必须是谓词命题，并且这个命题必须有真值
+% 这里的P实际上可以被看作是真值，如果P=true，那么则notP=false
+% 可以通过定义操作符来让not看起来更加自然，注意fx和fy的区别
+:-op(50, fy, not).
+not P:-
+P, !, fail;
+true.
+% 练习5-4,再candidate和ruleout表中找到在前者而不在后者的所有元素
+find_element([], _, []).
+
+find_element([X|Candidate], Ruleout, List):-
+member(X, Ruleout),
+find_element(Candidate, Ruleout, List);
+add1(X, List1, List),
+find_element(Candidate, Ruleout, List1).
+% 练习5-5
+difference(X, Y, Diff_list):-
+find_element(X, Y, Diff_list).
+% 总结：cut的两个作用
+% 1.可以改进程序的效率，告诉prolog别去试其他的可选选项，因为注定是错的
+% 2.cut可以适用于互斥的条件，最重要的是可以表示存在
+% 如果存在条件P则Q否则就R
+% cut分为红cut和绿cut，红cut对程序的陈述性意义有影响，而绿cut则没有
+% cut前面的目标相当于被锁定住了，一旦通过了这些目标并进入cut后，当匹配出错则直接返回头目标，并且不再回溯
